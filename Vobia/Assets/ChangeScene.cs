8d09b0851440f98f7642abae43625501fd1af0fa
Alarick Imperial <alarickimperial@gmail.com>
2018-03-19 20:54:46 -0400

    added more cubes

--- Vobia/Assets/ChangeScene.cs
+++ Vobia/Assets/ChangeScene.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void Changescene (int ChangingScene)
+    /// <summary>
+    /// Different Scene integers are found in the build setting, under build and run setting. then open the a new scene and it would generate a new integer
+    /// </summary>
+    /// <param name="ChangingScene"></param>
+    public void Changescene (int ChangingScene) //calling the int for the scene
     {
-        SceneManager.LoadScene(ChangingScene);
+        SceneManager.LoadScene(ChangingScene); //changing the scene from one another
     }
 	
 }
