646aa49d4636659e1ed760c51326f838426cbf0c
Alarick Imperial <alarickimperial@gmail.com>
2018-03-21 15:21:40 -0400

    Fixed the navigation, added various materials, fixed recticle pointer, compatible with GearVR.

---
+++ Vobia/Assets/Scripts/ColorChanges.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanges : MonoBehaviour {
+
+    /// <summary>
+    /// Color changes when the recticle is pointing to the object 
+    /// </summary>
+
+    public Material nolook; //declaring variables to both color changes
+    public Material looking;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Renderer>().material = nolook; //no action till pointed to the object
+    }
+
+    //function for looking at the object
+    public void Sight()
+    {
+        GetComponent<Renderer>().material = looking; 
+    }
+
+    //function for not looking at the object
+    public void Nosight()
+    {
+        GetComponent<Renderer>().material = nolook;
+    }
+}
