5c4289d8bad6bcdf7e86cc8512137850ebffd9ee
Alarick Imperial <alarickimperial@gmail.com>
2018-03-22 01:51:33 -0400

    Navigation is now fixed, added an animation for the cubes

--- Vobia/Assets/Nurface/GoogleVRAndGearVR/Scripts/VRBuildSettingsOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//[CreateAssetMenu(fileName = "VRBuildOptions", menuName = "Custom/VRBuildOptions", order = 1)]
-public class VRBuildSettingsOptions : ScriptableObject {
-    public bool enableCustomDefines;
-}
---
+++ Vobia/Assets/Scripts/animController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour {
+
+    /// <summary>
+    /// Providing Animation to a cube
+    /// </summary>
+    public Animator Anim;//call the variable Anim as a Animator
+    
+	// Use this for initialization
+	void Start ()
+    {
+        Anim = GetComponent<Animator>(); //declearing the anim as a animator
+    }
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        Anim.Play("CubeAnimation"); //calling the bool for the animation
+
+    }
+}
